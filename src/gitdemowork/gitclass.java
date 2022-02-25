04239ff2db643679bcc1146b978a93dfc1f5d002
Navya <kbnavya@gmail.com>
2022-02-25 15:04:21 +0530

    1st commit

---
+++ src/gitdemowork/gitclass.java
@@ -0,0 +1,24 @@
+package gitdemowork;
+
+public class gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi i am in git");
+
+		System.out.println("Hi i am in git");
+
+		System.out.println("Hi i am in git");
+		
+		System.out.println("Hi i am in git");
+		System.out.println("Hi i am in git");
+
+		System.out.println("Hi i am in git");
+
+		System.out.println("Hi i am in git");
+		
+		System.out.println("Hi i am in git");
+	}
+
+}
